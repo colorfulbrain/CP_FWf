cdef2cd36a4aa678993c95d2244d299e19194e1d
colorful_brain <33995342+colorfulbrain@users.noreply.github.com>
2024-06-23 16:35:48 +0800

    Update and rename 5_eddy.sh to 4_eddy_dtifit.sh

--- 4_eddy_dtifit.sh
+++ 4_eddy_dtifit.sh
@@ -2,8 +2,6 @@
 
 #subjDIR=$2
 #subjTAR=$3
-subjDIR="/home/binyin/nasdata/RJ_UI890_forAD/niigz"
-subjTAR='/home/binyin/nasdata/RJ_UI890_forAD/preproc'
 
 template='/home/binyin/local4t/research/codes/RJNBscripts/RJNBpreprocCode/templates'
 tool='/home/binyin/local4t/research/codes/RJNBscripts/RJNBpreprocCode/tools/'
@@ -29,16 +27,9 @@ done
 echo $indx > ./dMRI/eddy_index.txt
 
 
-## eddy
+## eddy correction
 
 $FSLDIR/bin/eddy_cuda10.2 --imain=./dMRI/PA.nii.gz --mask=./dMRI/nodif_brain_mask_ud.nii.gz --topup=./B0_AP_PA/fieldmap_out --acqp=./B0_AP_PA/acqparams.txt --index=./dMRI/eddy_index.txt --bvecs=./dMRI/bvecs --bvals=./dMRI/bvals --out=./dMRI/data --ref_scan_no=0 --flm=quadratic --resamp=jac --slm=linear --fwhm=2 --ff=5  --sep_offs_move --nvoxhp=1000 --very_verbose  --repol --rms
-
-
-# Description: Script to run GDC and select 1 cell on the dMRI data after eddy.
-#$tool/bb_GDC --workingdir=./data_GDC --in=./data.nii.gz --out=./data_ud.nii.gz --owarp=./data_ud_warp.nii.gz
-
-#Correct input for dtifit(The b=1000 shell is fed into dtifit)
-#$tool/bb_select_dwi_vols ./data_ud.nii.gz ./bvals ./data_ud_1_shell 1000 1 ./bvecs
 
 # ditfit for FA, MD, MO
 mkdir -p $subjTAR/$1/dMRI/dtifit
